cdaf7dca7e4063033a20d22d5ae10cf2c5d0cb90
kienen <kienenm14@gmail.com>
2022-03-02 20:09:04 -0500

    Solved issue where channels with custom url would not work.

--- yt_channel_titles_map/channel_titles.py
+++ yt_channel_titles_map/channel_titles.py
@@ -3,41 +3,7 @@ from wordcloud import WordCloud
 import re
 
 
-def find_all_titles(youtube, channel_name):
-    response = youtube.channels().list(
-        part="contentDetails",
-        forUsername=channel_name
-    ).execute()
-    items = response.get("items")[0]
-    content_details = items["contentDetails"]
-    channel_playlists = content_details["relatedPlaylists"]
-    uploads_id = channel_playlists["uploads"]
-    playlist_length = youtube.playlists().list(
-        part="contentDetails",
-        id=uploads_id,
-    ).execute().get("items")[0].get("contentDetails").get("itemCount")
 
-    counter = 0
-    next_page_token = ""
-    titles = []
-    while counter < playlist_length:
-        if counter + 50 > playlist_length:
-            result_amount = playlist_length - counter
-        else:
-            result_amount = 50
-
-        uploads_playlist = youtube.playlistItems().list(
-            part="snippet",
-            playlistId=uploads_id,
-            maxResults=result_amount,
-            pageToken=next_page_token
-        ).execute()
-        next_page_token = uploads_playlist.get("nextPageToken")
-        videos = uploads_playlist.get("items")
-        for video in videos:
-            titles.append(video.get("snippet").get("title"))
-        counter += 50
-    return titles
 
 
 def count_words(titles_list, stop_words):
--- yt_channel_titles_map/main.py
+++ yt_channel_titles_map/main.py
@@ -1,15 +1,15 @@
 # Input of project packages
 from setup_youtube_api import youtube_authenticate
-from channel_titles import find_all_titles, count_words, make_word_cloud
-
+from channel_titles import count_words, make_word_cloud
+from api_handler import find_channel_id, find_all_titles
 # Input of other packages
 from stop_words import get_stop_words
 
 if __name__ == '__main__':
     youtube = youtube_authenticate()
-    channel_name = input("Channel Name: ").strip()
-    titles = find_all_titles(youtube, channel_name)
+    channel_info = find_channel_id(youtube)
+    titles = find_all_titles(youtube, channel_info[0])
     stop_words = [w.upper() for w in get_stop_words("en")]
     title_words = count_words(titles, stop_words)
-    make_word_cloud(title_words, channel_name)
+    make_word_cloud(title_words, channel_info[1])
 
